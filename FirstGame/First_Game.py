2f38042e1a4f55474766b1e4c32f96be04d0858d
ShadowStorm0 <41564329+ShadowStorm0@users.noreply.github.com>
2021-10-07 22:58:45 -0500

    Basic Animations
    Signed-off-by: ShadowStorm0 <41564329+ShadowStorm0@users.noreply.github.com>

--- FirstGame/First_Game.py
+++ FirstGame/First_Game.py
@@ -11,6 +11,9 @@ sky_surface = pygame.image.load('./FirstGame/graphics/Sky.png')
 ground_surface = pygame.image.load('./FirstGame/graphics/ground.png')
 text_surface = test_font.render('First Game', False, 'Red')
 
+snail_surface = pygame.image.load('./FirstGame/graphics/snail/snail1.png')
+snail_x_pos = 600
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -20,6 +23,11 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
+    
+    snail_x_pos -= 4
+    if snail_x_pos < -100:
+        snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos, 250))
 
     #Draw all elements & Update everything
     pygame.display.update()
