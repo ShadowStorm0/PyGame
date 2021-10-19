854f7fc3979c369966b93863313a2b41313bd20f
ShadowStorm0 <41564329+ShadowStorm0@users.noreply.github.com>
2021-10-18 20:43:22 -0500

    Setup Screen, Quit and Font
    Signed-off-by: ShadowStorm0 <41564329+ShadowStorm0@users.noreply.github.com>

--- CSClub_GameJam #1/GameJam.py
+++ CSClub_GameJam #1/GameJam.py
@@ -1,12 +1,31 @@
 #By: ShadowStorm0 / Python47
 #Theme: Scale
 
-import pygame, random, secrets, choice, sys
+import pygame, random, secrets, sys
+from pygame.constants import *
 
 def main():
-    pass
+    # Starting PyGame
+    pygame.init()
+    screen = pygame.display.set_mode((800, 400))
+    pygame.display.set_caption('Expanding Space')
+    clock = pygame.time.Clock()
+    game_font = pygame.font.Font('./font/ThaleahFat.ttf', 50)
+    game_active = False
 
+    while True:
+        for event in pygame.event.get():
+            # Exits program with X button
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            # Exits with escape button
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    exit()
 
+  
 if __name__ == '__main__':
     main()
 else: 
