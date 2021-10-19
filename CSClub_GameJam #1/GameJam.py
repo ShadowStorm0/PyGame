be4a5a8b69a1901a0b19bae8db8f961b993ab074
ShadowStorm0 <41564329+ShadowStorm0@users.noreply.github.com>
2021-10-18 21:04:17 -0500

    Backbone code and change screen color logic
    Signed-off-by: ShadowStorm0 <41564329+ShadowStorm0@users.noreply.github.com>

--- CSClub_GameJam #1/GameJam.py
+++ CSClub_GameJam #1/GameJam.py
@@ -13,19 +13,51 @@ def main():
     game_font = pygame.font.Font('./font/ThaleahFat.ttf', 50)
     game_active = False
 
+    # Groups
+    
+    # Background(s)
+    
+    # Intro screen
+    
+    # Timers
+
     while True:
         for event in pygame.event.get():
             # Exits program with X button
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+                
             # Exits with escape button
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     pygame.quit()
                     exit()
+                    
+            # Start and restart game
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
 
-  
+        #Obstacle selection (use secrets)
+
+
+        if game_active:
+            screen.fill((0,0,255))
+            # Draw Sky, and Ground
+            
+            # Draw Player
+            
+            # Draw Obstacle(s)
+            
+            # Collision with Obstacle(s)
+        else:
+            screen.fill((94, 129, 162))
+            
+        
+        # Draw all elements and update everything
+        pygame.display.update()
+        clock.tick(60)
+            
 if __name__ == '__main__':
     main()
 else: 
